e500974a63d5875ec92ad46b57511c9ba86dd557
Arthur Denner <arthurdenner7@gmail.com>
2018-06-12 14:04:48 -0300

    test: Improving arguments validation and adding more tests (#1)

--- react-puer.js
+++ react-puer.js
@@ -1,17 +1,32 @@
-const NODE_ENV = process.env.NODE_ENV;
+const { NODE_ENV } = process.env;
 
-const isFunction = prop => typeof prop === "function";
+const isFunction = prop => typeof prop === 'function';
+const isObject = prop => typeof prop === 'object';
 
 const warn = (condition, message) =>
   condition && console.warn(`[react-puer]: ${message}`);
 
-export default ({ children, render }, ...props) => {
+const error = (condition, message) => {
+  if (condition) {
+    throw new Error(`[react-puer]: ${message}`);
+  }
+};
+
+export default ({ children, render } = {}, props = {}) => {
+  error(
+    !children && !render,
+    'You need pass an object with a `children` or `render` property'
+  );
+  error(children && !isFunction(children), '`children` must be a function');
+  error(render && !isFunction(render), '`render` must be a function');
+  error(props && !isObject(props), '`props` must be an object');
+
   warn(
-    NODE_ENV !== "production" && isFunction(children) && isFunction(render),
-    "You cant use children and render, so only children will be used."
+    NODE_ENV !== 'production' && isFunction(children) && isFunction(render),
+    "You can't use children and render, so only children will be used"
   );
 
   const reactPuer = isFunction(children) ? children : render;
 
-  return reactPuer(...props);
+  return reactPuer(props);
 };
